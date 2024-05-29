b3fb49953d0878b2610bef644b430b9c0a69552e
Daniel Amaya <damayalop@gmail.com>
2024-05-28 21:53:47 -0600

    Add new challenge

---
+++ src/pages/contact-form-main/main.js
@@ -0,0 +1,53 @@
+const $form = document.querySelector('#form')
+const $textError = document.querySelectorAll('.text-error')
+const $btnSubmit = document.querySelector('#btn-submit')
+const $alertCard = document.querySelector('.alert')
+let showSubmitError = false
+
+$form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const data = new FormData($form)
+
+  if (!data.has('config') || !data.has('querytype')) {
+    showSubmitError = true
+    $textError.forEach((el) => el.classList.add('show-error'))
+    return
+  }
+
+  for (const value of data.values()) {
+    if (value === '') {
+      showSubmitError = true
+      $textError.forEach((el) => el.classList.add('show-error'))
+      return
+    }
+  }
+
+  if (showSubmitError) {
+    showSubmitError = false
+    $textError.forEach((el) => el.classList.remove('show-error'))
+  }
+
+  ToggleEnabledForm($form)
+  $btnSubmit.innerHTML = 'Enviando...'
+
+  setTimeout(() => {
+    $alertCard.classList.add('show-alert')
+  }, 2000)
+
+  setTimeout(() => {
+    $form.reset()
+    $btnSubmit.innerHTML = 'Submit'
+    ToggleEnabledForm($form)
+  }, 3000)
+
+  setTimeout(() => {
+    $alertCard.classList.remove('show-alert')
+  }, 4000)
+})
+
+function ToggleEnabledForm(form) {
+  Array.from(form.elements).forEach((formElement) => {
+    formElement.disabled = !formElement.disabled
+    formElement.classList.toggle('disabled')
+  })
+}
