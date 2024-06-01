21d871613109c84dde55b3b84e4dc0c7fbd4b3d7
Daniel Amaya <damayalop@gmail.com>
2024-05-31 23:13:44 -0600

    Update index time-tracking-dashboard

--- src/pages/time-tracking-dashboard/index.js
+++ src/pages/time-tracking-dashboard/index.js
@@ -1,17 +1,54 @@
 class TimeTrackingCard extends HTMLElement {
+  categories = {
+    work: {
+      name: 'Work',
+      icon: 'work'
+    },
+    play: {
+      name: 'Play',
+      icon: 'play'
+    },
+    study: {
+      name: 'Study',
+      icon: 'study'
+    },
+    exercise: {
+      name: 'Exercise',
+      icon: 'exercise'
+    },
+    social: {
+      name: 'Social',
+      icon: 'social'
+    },
+    self_care: {
+      name: 'Self Care',
+      icon: 'self_care'
+    }
+  }
+
   constructor() {
     super()
 
-    this.category = this.getAttribute('category') ?? 'default'
+    this.category =
+      this.categories[this.getAttribute('category')] ?? this.categories.work
 
     this.innerHTML = /*html*/ `
         <style>${TimeTrackingCard.style}</style>
         <div class="card">
             <div class="card-container">
                 <div class="card-nav">
-                <h4 class="card-title">${this.category}</h4>
-                <img src="./assets/icon-ellipsis.svg" alt="" />
-            </div>
+                <h4 class="card-title">${this.category.name}</h4>
+                <button>
+                    <span class="sr-only">options</span>
+                    <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                    />
+                    </svg>
+                </button>
+                </div>
                 <h1 class="card-current-time">32hrs</h1>
                 <p class="card-previous-time">Last Week - 36hr</p>
             </div>
@@ -23,7 +60,7 @@ class TimeTrackingCard extends HTMLElement {
     return /*css*/ `
         @scope {
             .card {
-                --size-x: 300px;
+                --size-x: 280px;
                 --size-y: 200px;
 
                 position: relative;
@@ -54,7 +91,7 @@ class TimeTrackingCard extends HTMLElement {
                 background-image: url('./assets/icon-work.svg');
                 background-repeat: no-repeat;
                 background-position: right;
-                background-position-x: 190px;
+                background-position-x: 90%;
                 background-position-y: -10px;
                 background-size: 90px;
             }
@@ -79,14 +116,42 @@ class TimeTrackingCard extends HTMLElement {
             }
 
             .card-current-time {
-                font-size: 52px;
+                font-size: 54px;
                 font-weight: 300;
             }
 
             .card-previous-time {
-                font-size: 14px;
+                font-size: 16px;
                 font-weight: 400;
                 color: var(--pale-blue);
+            }
+
+            button {
+                background: transparent;
+                border: none;
+                cursor: pointer;
+                
+                &>svg {
+                    width: 30px;
+                    height: 10px;
+                    color: #BBC0FF;
+
+                    &:hover {
+                        color: white;
+                    }
+                }   
+            }
+
+            .sr-only {
+                position: absolute;
+                width: 1px;
+                height: 1px;
+                padding: 0;
+                margin: -1px;
+                overflow: hidden;
+                clip: rect(0, 0, 0, 0);
+                white-space: nowrap;
+                border-width: 0;
             }
         }
     `
